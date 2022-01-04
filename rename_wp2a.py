6bd8e911c9f365274c1d465a6aad0c57c4f2b952
Davi93 <dxa869@student.bham.ac.uk>
2022-01-04 13:08:58 +0000

    script updated

--- rename_wp2a.py
+++ rename_wp2a.py
@@ -12,17 +12,16 @@ main_folder = 'D:/roast-chapter3/wp2a/'
 for subdir, dirs, files in os.walk(main_folder):
     if 'sub' in subdir: # iterating subjects' folders
         sub = subdir.split('-')[2] # subject number
-        if sub != '01': # I had already renamed sub-01's files manually
+        if (sub != '01') & (sub != '02') & (sub != '03'): # I had already renamed files for sub 01 02 03.
             print (sub)
             os.chdir(subdir)
             for file in files:
                     print (os.path.join(subdir, file))
-                    if sub == '03':
-                        if '.json' in file:
-                            os.rename(file,'sub-'+sub+'_T1.json')
-                        else:
-                            if 'T1' in file:
-                                os.rename(file,'sub-'+sub+'_T1.nii')
-                            if 't2' in file:
-                                os.rename(file,'sub-'+sub+'_T2.nii')
-                            
+                    if '.json' in file:
+                        os.rename(file,'sub-'+sub+'_T1.json')
+                    else:
+                        if 'T1' in file:
+                            os.rename(file,'sub-'+sub+'_T1.nii')
+                        if 't2' in file:
+                            os.rename(file,'sub-'+sub+'_T2.nii')
+                        
