98341feefbbe23d4512ef71a1da5760a09451464
Davi93 <dxa869@student.bham.ac.uk>
2022-01-07 14:08:54 +0000

    peb pp fixed

--- posterior_probability_wp2a_day1_persubject.m
+++ posterior_probability_wp2a_day1_persubject.m
@@ -7,9 +7,9 @@ load PEBppA.mat
 
 BMAs = {}
 
-for i = 1:3:66
+for i = 1:66
    this_subject_BMA = spm_dcm_peb_bmc(PEBpp{i})
    BMAs{end+1} = this_subject_BMA
 end
-BMAs = BMAs.'
+
 save('Day1_all_Pp.mat','BMAs')
