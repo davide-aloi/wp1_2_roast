b6f375731d6e8eb63dd8f82a4254488c88e47801
Davi93 <dxa869@student.bham.ac.uk>
2022-01-07 13:40:55 +0000

    I was reading the wrong EP values

--- wp2a_extract_single_dcm_vals.m
+++ wp2a_extract_single_dcm_vals.m
@@ -1,21 +1,7 @@
 %Script to extract pre vs post EP values for each subject 
 % I am going to use this to plot responders vs non responders EP values 
 clear all
-cd C:\Users\davide\Documents\GitHub\WP2A_joystick\PEB_nov2021\results_wp2a_pairwise_day1only\
-load('PEBpairwise_day1A.mat')
-
-ALLEP = {}
-
-for i = 1:length(PEBpairwise_day1)
-    thisEPVals = PEBpairwise_day1{i,1}.Ep(17:32)
-    ALLEP{i} = thisEPVals
-end
-
-save('Day1_all_EPvalues.mat','ALLEP')
-
-
-clear all
-cd C:\Users\davide\Documents\GitHub\WP2A_joystick\friday_session\DCM_friday_session\results\PEBfullAB_all
+cd C:\Users\davide\Documents\GitHub\wp1_2_roast\
 load('PEBppA.mat')
 
 ALLEP = {}
@@ -25,4 +11,4 @@ for i = 1:length(PEBpp)
     ALLEP{i} = thisEPVals
 end
 
-save('Day5_all_EPvalues.mat','ALLEP')
+save('Day1_all_EPvalues.mat','ALLEP')
