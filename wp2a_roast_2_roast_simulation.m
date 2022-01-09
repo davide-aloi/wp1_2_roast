00b53f2c2612dd54af321c909bca8f3d6ec6df1a
Davi93 <dxa869@student.bham.ac.uk>
2022-01-09 18:01:31 +0000

    stuff changed

--- wp2a_roast_2_roast_simulation.m
+++ wp2a_roast_2_roast_simulation.m
@@ -2,8 +2,7 @@
 data_path = 'D:\roast-chapter3\wp2a\';
 
 %List of Wp2a participants that have both a T1 and a T2
-%participants = {'01','02','03','04','06','07','08','09','10','11','12','13','14','15','17','18','19','20','22','23','24'}
-participants = {'06'}
+participants = {'01','02','03','04','06','07','08','09','10','11','12','13','14','15','17','18','19','20','22','23','24'}
 
 %List of participants with only t1
 participants_onlyt2 = {'16'}
@@ -25,4 +24,3 @@ for i = 1:length(participants_onlyt2)
     t1 = fullfile(folder,strcat('sub-',p,'_T1.nii'))
     roast(t1,{'C3',1.0,'Fp2',-1.0},'electype', 'pad', 'elecsize',[50 50 3],'capType','1020')
 end
-
