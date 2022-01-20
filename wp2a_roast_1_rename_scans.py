db1df8b5d9550c150659dbce02fed26ccfdf421d
Davi93 <dxa869@student.bham.ac.uk>
2022-01-20 13:33:48 +0000

    line of code fixed

--- wp2a_roast_1_rename_scans.py
+++ wp2a_roast_1_rename_scans.py
@@ -21,12 +21,12 @@ main_folder = 'D:/roast-chapter3/wp2a/'
 for subdir, dirs, files in os.walk(main_folder):
     if 'sub' in subdir: # iterating subjects' folders
         sub = subdir.split('-')[2] # subject number
-            print (sub)
-            os.chdir(subdir)
-            for file in files:
-                    print (os.path.join(subdir, file))
-                    if '.json' in file:
-                        os.rename(file,'sub-' + sub + '_T1.json')
-                    else:
-                        if 'T1' in file:
-                            os.rename(file,'sub-' + sub + '_T1.nii')
+        print(sub)
+        os.chdir(subdir)
+        for file in files:
+                print(os.path.join(subdir, file))
+                if '.json' in file:
+                    os.rename(file,'sub-' + sub + '_T1.json')
+                else:
+                    if 'T1' in file:
+                        os.rename(file,'sub-' + sub + '_T1.nii')
