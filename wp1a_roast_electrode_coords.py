8b664c76278c0ebcc8de719e9af82260bd173bbf
Davi93 <dxa869@student.bham.ac.uk>
2022-02-10 16:54:43 +0000

    comments added

--- wp1a_roast_electrode_coords.py
+++ wp1a_roast_electrode_coords.py
@@ -3,6 +3,7 @@
 # identified using MRIcronGL. The script also iterates through the folders and saves a file
 # .txt named customLocations.txt containing the coordinates for the active and reference
 # electrodes. 
+import os 
 
 main_folder = 'C:\\Users\\davide\\Documents\\GitHub\\wp1_2_roast\\' 
 output_folder = 'D:\\roast-chapter3\\wp_all_results\\' # where to save results
@@ -13,8 +14,6 @@ db_names = ['wp1a']
 db_subjects = [['03','04','05','07','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26']]
 
 # List of coordinates for each subject (return electrode: cathodal, active electrode: anodal)
-
-
 anod_coords = {
             '03': (36,117,204),
             '04': (46,131,228),
@@ -39,7 +38,6 @@ anod_coords = {
             '25': (32,130,183),
             '26': (26,141,183)
 }
-
 
 cath_coords = {
             '03': (119,185,190),
@@ -72,7 +70,7 @@ cath_coords = {
 for db_id, db in enumerate(db_names): # Iterate all three datasets
     db_path = os.path.join(data_folder, db) # Dataset folder
 
-    for i, subject in tqdm(enumerate(db_subjects[db_id])): #Iterate all subjects
+    for i, subject in enumerate(db_subjects[db_id]): #Iterate all subjects
         path = db_path + '\sub-' + subject # Subject folder
         print(path)
         fname = 'customLocations.txt'
